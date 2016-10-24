33cf6b2d0be971eae988bc2404b60674b4157561
eric tsiliacos <erictsiliacos@gmail.com>
2016-10-24 16:46:05 -0700

    Extract out Model/Update/View

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,27 @@
-import Html exposing (text)
+module Main exposing (..)
+
+import Html exposing (Html, text)
 import Html.App exposing (beginnerProgram)
 
 
+type alias Model =
+    {}
+
+
+update : msg -> model -> model
+update _ model =
+    model
+
+
+view : model -> Html msg
+view _ =
+    text "Hello World!"
+
+
 main : Program Never
-main = beginnerProgram
-    { model = {}
-    , update = \msg model -> {}
-    , view = \_ -> text "Hello World!"
-    }
+main =
+    beginnerProgram
+        { model = {}
+        , update = update
+        , view = view
+        }
